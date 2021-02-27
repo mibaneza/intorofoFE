a0fe9c11cfa5c54192f7f3afeee8120fe4f078f6
mibaneza <mibaneza@ucvvirtual.edu.pe>
2021-02-27 00:48:43 -0500

    v1.0005

--- src/app/authCodeFlowConfig.config.ts
+++ src/app/authCodeFlowConfig.config.ts
@@ -3,7 +3,7 @@ import {AuthConfig} from 'angular-oauth2-oidc';
 export const authCodeFlowConfig: AuthConfig = {
   loginUrl: 'https://discord.com/api/oauth2/authorize',
   tokenEndpoint: 'https://discord.com/api/oauth2/token',
-  redirectUri: 'http://localhost:4200/forums/?',
+  redirectUri: 'http://localhost:4200/#',
   clientId: '788516512600293426',
   scope: 'identify',
   responseType: 'token',
